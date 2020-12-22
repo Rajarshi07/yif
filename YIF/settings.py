579365e144596eeb8f05139806630669cba93c1e
rajarshi07 <rajarshighoshdastidar@gmail.com>
2020-12-22 14:55:31 +0530

    Commit2

--- YIF/settings.py
+++ YIF/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field',
+    # 'phonenumber_field',
     'app',
 ]
 
